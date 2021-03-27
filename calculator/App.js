212ff0f0f9956c5cd3990d7e583898bf8be8ddd7
Pedro Henrique Franca de Deus <pedrohenriquefrancadedeus@gmail.com>
2021-03-26 21:05:49 -0300

    under construction

--- calculator/App.js
+++ calculator/App.js
@@ -1,22 +1,131 @@
-import React from 'react';
-import { Text, StatusBar } from 'react-native';
-import {Container, Calc, AreaButton, GreyButton} from './styles';
+import React, { useState } from 'react';
+import { StatusBar } from 'react-native';
+import {
+  Container, 
+  Calc, 
+  AreaButton, 
+  GreyButton, 
+  OrangeButton,
+  DarkButton,
+  LargeButton,
+  WhiteText, 
+  DarkText
+} from './styles';
 
 const App = () => {
+  const [ num, setNum ] = useState("0");
+
   return (
     <Container>
       <StatusBar backgroundColor={"#000"} />
-      <Calc>0</Calc>
+      <Calc>{num}</Calc>
+
       <AreaButton>
-        <GreyButton>
-          <Text>AC</Text>
+        <GreyButton onPress={() => {
+          setNum(0);
+        }}>
+          <DarkText>AC</DarkText>
         </GreyButton>
+
         <GreyButton>
-          <Text>AC</Text>
+          <DarkText> +/- </DarkText>
         </GreyButton>
+
         <GreyButton>
-          <Text>AC</Text>
+          <DarkText>%</DarkText>
         </GreyButton>
+
+        <OrangeButton>
+          <WhiteText>/</WhiteText>
+        </OrangeButton>
+      </AreaButton>
+
+      <AreaButton>
+        <DarkButton>
+          <WhiteText onPress={() => {
+          setNum([num + '7']);
+        }}>7</WhiteText>
+        </DarkButton>
+
+        <DarkButton>
+          <WhiteText onPress={() => {
+          setNum([num + '8']);
+        }}>8</WhiteText>
+        </DarkButton>
+
+        <DarkButton>
+          <WhiteText onPress={() => {
+          setNum([num + '9']);
+        }}>9</WhiteText>
+        </DarkButton>
+
+        <OrangeButton>
+          <WhiteText>X</WhiteText>
+        </OrangeButton>
+      </AreaButton>
+
+      <AreaButton>
+        <DarkButton onPress={() => {
+          setNum([num + '4']);
+        }}>
+          <WhiteText>4</WhiteText>
+        </DarkButton>
+
+        <DarkButton onPress={() => {
+          setNum([num + '5']);
+        }}>
+          <WhiteText>5</WhiteText>
+        </DarkButton>
+
+        <DarkButton onPress={() => {
+          setNum([num + '6']);
+        }}>
+          <WhiteText>6</WhiteText>
+        </DarkButton>
+
+        <OrangeButton>
+          <WhiteText>-</WhiteText>
+        </OrangeButton>
+      </AreaButton>
+
+      <AreaButton>
+        <DarkButton onPress={() => {
+          setNum([num + '1']);
+        }}>
+          <WhiteText>1</WhiteText>
+        </DarkButton>
+
+        <DarkButton onPress={() => {
+          setNum([num + '2']);
+        }}>
+          <WhiteText>2</WhiteText>
+        </DarkButton>
+
+        <DarkButton onPress={() => {
+          setNum([num + '3']);
+        }}>
+          <WhiteText>3</WhiteText>
+        </DarkButton>
+
+        <OrangeButton>
+          <WhiteText>+</WhiteText>
+        </OrangeButton>
+      </AreaButton>
+
+      <AreaButton>
+        <LargeButton onPress={() => {
+          setNum([num + '0']);
+        }}>
+          <WhiteText>0</WhiteText>
+        </LargeButton>
+
+        <DarkButton>
+          <WhiteText>,</WhiteText>
+        </DarkButton>
+
+        <OrangeButton>
+          <WhiteText>=</WhiteText>
+        </OrangeButton>
       </AreaButton>
     </Container>
   );
--- calculator/styles.js
+++ calculator/styles.js
@@ -7,21 +7,65 @@ export const Container = styled.View`
 
 export const Calc = styled.Text`
   color: #fff;
-  font-size: 100px;
-  font-weight: 100;
+  font-size: 120px;
   text-align: right;
-  margin-top: 100px;
+  margin-top: 150px;
+  margin-right: 30px;
+  margin-bottom: 30px;
 `;
 
 export const AreaButton = styled.View`
-  flex: 1;
+  flex-direction: row;
+  margin-bottom: 20px;
 `;
 
 export const GreyButton = styled.TouchableOpacity`
   background-color: #a5a5a5;
-  padding: 25px;
-  margin-left: 25px;
-  margin-right: 25px;
+  margin-left: 20px;
   width: 78px;
   height: 78px;
+  border-radius: 100px;
+  justify-content: center;
+`;
+
+export const OrangeButton = styled.TouchableOpacity`
+  background-color: #FE9E09;
+  margin-left: 20px;
+  width: 78px;
+  height: 78px;
+  border-radius: 100px;
+  justify-content: center;
+`;
+
+export const DarkButton = styled.TouchableOpacity`
+  background-color: #333333;
+  margin-left: 20px;
+  width: 78px;
+  height: 78px;
+  border-radius: 100px;
+  justify-content: center;
+`;
+
+export const LargeButton = styled.TouchableOpacity`
+  background-color: #333333;
+  margin-left: 20px;
+  padding-left: 33px;
+  width: 176px;
+  height: 78px;
+  border-radius: 100px;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const WhiteText = styled.Text`
+  text-align: center;
+  font-size: 35px;
+  font-weight: 600;
+  color: #fff;
+`;
+
+export const DarkText = styled.Text`
+  text-align: center;
+  font-size: 35px;
+  font-weight: 600;
 `;
